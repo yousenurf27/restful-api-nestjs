7751d6e8073311de5aced21562122f6f44ef33bb
yousenurf27 <yosenurf@gmail.com>
2024-10-02 04:42:25 +0700

    refactor: modify root api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 
-@Controller('/api')
+@Controller('')
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Welcome to Management Users RESTful API';
+    return 'Welcome to Management Users RESTful API, read doc in my GitHub https://github.com/yousenurf27/restful-api-nestjs';
   }
 }
