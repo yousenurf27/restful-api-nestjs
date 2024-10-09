de6d3f2ea7af657df34de94062dab8b11f66ed7f
yousenurf27 <yosenurf@gmail.com>
2024-10-09 08:33:42 +0700

    refactor: :fire: remove unnecessary controller

--- src/app.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('')
-export class AppController {
-  @Get()
-  getHello(): string {
-    return 'Welcome to Management Users RESTful API, read doc in my GitHub https://github.com/yousenurf27/restful-api-nestjs';
-  }
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from './common/common.module';
 import { UserModule } from './user/user.module';
 import { ContactModule } from './contact/contact.module';
 import { AddressModule } from './address/address.module';
-import { AppController } from './app.controller';
 
 @Module({
   imports: [CommonModule, UserModule, ContactModule, AddressModule],
-  controllers: [AppController],
+  controllers: [],
   providers: [],
 })
 export class AppModule {}
